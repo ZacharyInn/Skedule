37e68221a4417dd4c8eba5864ec08088a7482e5d
Zachary Inn <72847185+ZacharyInn@users.noreply.github.com>
2022-03-02 12:07:33 -0800

    Update break.cpp

--- break.cpp
+++ break.cpp
@@ -1,1 +1,33 @@
+#include "Break.h"
 
+Break::Break(){}
+Break::~Break(){}
+
+// getters
+int Break::getLength()
+{
+  return length;
+}
+int Break::getTime()
+{
+  return time;
+}
+int Break::getNumBreaks()
+{
+  return numBreaks;
+}
+
+
+// setters
+void Break::setLength(int l)
+{
+  length = l;
+}
+void Break::setTime(int t)
+{
+  time = t;
+}
+void Break::setNumBreaks(int n)
+{
+  numBreaks = n;
+}
